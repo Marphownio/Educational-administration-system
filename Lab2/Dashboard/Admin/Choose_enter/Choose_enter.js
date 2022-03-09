78c4dd968ac1111ff31649ee2f9cc42c2b627d05
Lu Yiyang <20300200013@fudan.edu.cn>
2022-03-09 12:23:54 +0800

    管理员页面、添加信息页面

---
+++ Lab2/Dashboard/Admin/Choose_enter/Choose_enter.js
@@ -0,0 +1,11 @@
+
+
+var add = document.getElementById('Add');
+var hide = document.getElementById('hide');
+var CloseHide = document.getElementById('Closehide');
+hide.onclick = function () {
+    box.style.display = "block";
+}
+Closehide.onclick = function(){
+    box.style.display = "none";
+}
