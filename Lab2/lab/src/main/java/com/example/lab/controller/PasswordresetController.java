e487ac46e804321fa75639708df511eddc810751
Mai Wuyuao <20300290032@fudan.edu.cn>
2022-03-14 20:12:27 +0800

    修改resetpassword

---
+++ Lab2/lab/src/main/java/com/example/lab/controller/PasswordresetController.java
@@ -0,0 +1,28 @@
+package com.example.lab.controller;
+
+import com.example.lab.pojo.User;
+import com.example.lab.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class PasswordresetController {
+    @Resource
+    private UserService userService;
+    @GetMapping(value = "/passwordreset")
+    public String login(@RequestParam("newPassword1") String newPassword1, @RequestParam("newPassword2") String newPassword2, Model model, HttpSession session) {
+        if (!newPassword1.equals(newPassword2)) {
+            model.addAttribute("msg1", "请两次输入的密码保持一致");
+            return "/resetPassword";
+        }
+        else if(newPassword1.equals("123456")) {
+            model.addAttribute("msg1", "请输入与初始密码不同的新密码");
+            return "/resetPassword";
+        }
+    }
+}
