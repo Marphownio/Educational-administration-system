0da037515912aebde8a3e2100b8c0dddb75e18e3
Mai Wuyuao <20300290032@fudan.edu.cn>
2022-03-15 01:20:14 +0800

    重置密码

--- Lab2/lab/src/main/java/com/example/lab/controller/resetPasswordController.java
+++ Lab2/lab/src/main/java/com/example/lab/controller/resetPasswordController.java
@@ -2,10 +2,13 @@ package com.example.lab.controller;
 
 import com.example.lab.pojo.User;
 import com.example.lab.service.UserService;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import javax.servlet.http.HttpServletRequest;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
@@ -14,9 +17,10 @@ import javax.servlet.http.HttpSession;
 public class resetPasswordController {
     @Resource
     private UserService userService;
+    @NotNull HttpServletRequest request;
     @GetMapping (value ="resetPassword")
     public String resetPassword(@RequestParam("newPassword1") String newPassword1, @RequestParam("newPassword2") String newPassword2, Model model, HttpSession session) {
-        String id = (String) session.getAttribute("User");
+        String id=(String)request.getSession().getAttribute("user");
         if (!newPassword1.equals(newPassword2)) {
             model.addAttribute("msg1", "请两次输入的密码保持一致");
             return "/resetPassword";
