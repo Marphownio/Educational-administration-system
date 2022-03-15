4f3e5fe2fcfc7466908967842a365bd976b4a83c
huleyao <20302010042@fudan.edu.cn>
2022-03-15 19:23:47 +0800

    修改user表格

--- Lab2/lab/src/main/resources/user.sql
+++ Lab2/lab/src/main/resources/user.sql
@@ -1,12 +1,12 @@
 create table if not exists user
 (
-    id           varchar(8)     not null unique primary key,
+    id           varchar(8)  not null unique primary key,
     password     varchar(32) not null default '123456',
     name         varchar(32) not null,
     role         int         not null,
-    id_number    varchar(18)    not null unique,
-    phone_number varchar(11) unique,
-    email        varchar(32) unique
+    id_number    varchar(18) not null,
+    phone_number varchar(11),
+    email        varchar(32)
 );
 
 replace into user(id, password, name, role, id_number) values (0, 'admin', 'admin', 0, 'admin');
