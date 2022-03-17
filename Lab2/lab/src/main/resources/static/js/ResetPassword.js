89d0581ffdb280f669fb90aa6a5a355c1c1d7232
Mai Wuyuao <20300290032@fudan.edu.cn>
2022-03-17 10:21:15 +0800

    resetpassword修改，验证改为js

--- Lab2/lab/src/main/resources/static/js/ResetPassword.js
+++ Lab2/lab/src/main/resources/static/js/ResetPassword.js
@@ -2,16 +2,42 @@ function testpassword(){
     const password1Obj=document.getElementById("newPassword1");
     const password1=password1Obj.value;
     const warn_msgObj=document.getElementById("warn_msg");
-    const pat0=/^[\w-_]{6,32}$/;//密码的规则
-    const pat1=/^[a-zA-Z0-9]{6,32}$/;//密码的规则
-    const pat2=/^[0-9-_]{6,32}$/;//密码的规则
-    const pat3=/^[-_a-zA-Z]{6,32}$/;//密码的规则
-    if(!pat1.test(password1)&&!pat2.test(password1)&&!pat3.test(password1)&&!pat0.test(password1)){
+    const pat0=/^[\w-_]{6,32}$/;//密码的规则0
+    const pat1=/^[0-9]{6,32}$/;//密码的规则1
+    const pat2=/^[-_]{6,32}$/;//密码的规则2
+    const pat3=/^[a-zA-Z]{6,32}$/;//密码的规则3
+    if(pat1.test(password1)||pat2.test(password1)||pat3.test(password1)){
         warn_msgObj.innerText="*密码需长度为6-32位，且字母，数字或者特殊字符（-_）至少包含两种";
         return false;
     }
     else {
-        warn_msgObj.innerText="&nbsp;";
+        if(pat0.test(password1)){
+            warn_msgObj.innerText="";
+            return true;
+        }
+        else {
+            warn_msgObj.innerText = "*密码需长度为6-32位，且字母，数字或者特殊字符（-_）至少包含两种";
+            return false;
+        }
+    }
+}
+
+function WhetherSame(){
+    const password1Obj=document.getElementById("newPassword1");
+    const password1=password1Obj.value;
+    const password2Obj=document.getElementById("newPassword2");
+    const password2=password2Obj.value;
+    if(password1==password2){
         return true;
     }
+    else{
+        const warn_msgObj=document.getElementById("warn_msg");
+        warn_msgObj.innerText="*请两次输入的密码保持一致";
+        return false;
+    }
+}
+
+function submit_check(){
+    if(testpassword()==true&&WhetherSame()==true) return true;
+    else return false;
 }
