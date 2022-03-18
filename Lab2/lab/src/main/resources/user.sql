25ddd0ce68014caaea795565236d02058ccb4b70
Mai Wuyuao <20300290032@fudan.edu.cn>
2022-03-18 14:29:56 +0800

    取消电话号码以及邮箱的unique

--- Lab2/lab/src/main/resources/user.sql
+++ Lab2/lab/src/main/resources/user.sql
@@ -5,8 +5,8 @@ create table if not exists user
     name         varchar(32) not null,
     role         int         not null,
     id_number    varchar(18) not null unique,
-    phone_number varchar(11) unique,
-    email        varchar(32) unique
+    phone_number varchar(11),
+    email        varchar(32)
 );
 
 replace into user(id, password, name, role, id_number) values (000000, 'fudan_admin', 'admin', 0, 'admin');
