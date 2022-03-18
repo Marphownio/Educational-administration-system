ff46c4c196dbe7e0a6a7d4020cfb5662870ae1c6
Mai Wuyuao <20300290032@fudan.edu.cn>
2022-03-18 18:23:07 +0800

    致命错误修改

--- Lab2/lab/src/main/java/com/example/lab/controller/LoginController.java
+++ Lab2/lab/src/main/java/com/example/lab/controller/LoginController.java
@@ -19,20 +19,20 @@ public class LoginController {
 
     // 登录
     @RequestMapping(value = "/login")
-    public String login(@RequestParam("id") String id, @RequestParam("password") String password, Model model, HttpSession session) {
+    public String login(@RequestParam("id") String id, @RequestParam("password") String getpassword, Model model, HttpSession session) {
         if(!id.matches("^\\d{6}$") && !id.matches("^\\d{8}$")){
             model.addAttribute("msg","用户名或密码错误");
             return "login";
         }
         User user = userService.findUserById(parseInt(id));
-        if (user == null || !user.getPassword().equals(password)) {
+        if (user == null || !user.getPassword().equals(getpassword)) {
             model.addAttribute("msg", "用户名或密码错误");
             return "login";
         }
         else {
             session.setAttribute("user", user);
             // 初次登录，需要重置密码
-            if (password.equals("fudan123456")) {
+            if (getpassword.equals("fudan123456")) {
                 return "resetPassword";
             }
             // 跳转到首页
