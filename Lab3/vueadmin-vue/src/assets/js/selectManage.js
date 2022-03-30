f8cae804f3c943f1fc34c0281d362ceefd27c8b4
Lu Yiyang <20300200013@fudan.edu.cn>
2022-03-30 11:42:18 +0800

    选课页面修改

---
+++ Lab3/vueadmin-vue/src/assets/js/selectManage.js
@@ -0,0 +1,15 @@
+import Nav from "@/views/inc/Nav.vue";
+
+export default {
+    name: "selectManage",
+    components:{
+        Nav
+    },
+    methods:{
+        openSelection(){
+        },
+        closeSelection(){
+
+        }
+    }
+}
--- Lab3/vueadmin-vue/src/views/sys/admin/selectManage.vue
+++ Lab3/vueadmin-vue/src/views/sys/admin/selectManage.vue
@@ -1,11 +1,19 @@
 <template>
+    <head>
+        <meta charset="UTF-8">
+        <title>课程管理</title>
+    </head>
 
+    <body>
+
+    <Nav></Nav>
+    <el-button @click="openSelection" style="background-color: #59c2c5;color: white;margin: 0 auto;">开放选课</el-button>
+    <el-button @click="closeSelection" style="background-color: crimson;color: white;margin: 0 auto">关闭选课</el-button>
+    </body>
 </template>
 
-<script>
-    export default {
-        name: ""
-    }
+<script src="../../../assets/js/selectManage.js" type="text/javascript">
+
 </script>
 
 <style scoped>
