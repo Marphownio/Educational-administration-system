1a221a5e11e51a561b850f792aa314cecccb6393
huleyao <20302010042@fudan.edu.cn>
2022-04-05 05:05:27 +0800

    前后端跨域完善

--- Vue/src/views/Login.vue
+++ Vue/src/views/Login.vue
@@ -18,10 +18,10 @@
       <h4 style="text-align:center">请先登录</h4>
       <p style="color:red" text="${msg}" if="${not #strings.isEmpty(msg)}"></p>
 
-      <el-form-item class="typein" label="学/工号:" prop="loginid" ref="loginid" id="loginid">
+      <el-form-item class="typein" label="学/工号:" prop="loginid" ref="loginid">
         <el-input v-model="loginruleForm.loginid"></el-input>
       </el-form-item>
-      <el-form-item class="typein" label="密&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;码:" prop="loginpw" ref="loginpw" id="loginpw">
+      <el-form-item class="typein" label="密&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;码:" prop="loginpw" ref="loginpw">
         <el-input  v-model="loginruleForm.loginpw"></el-input>
       </el-form-item>
       <el-form-item>
