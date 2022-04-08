2124edf33a500739c839a9c4fb55730f0465aa18
Mai Wuyuao <20300290032@fudan.edu.cn>
2022-04-08 13:25:07 +0800

    实现登录功能

--- Vue/src/assets/js/login.js
+++ Vue/src/assets/js/login.js
@@ -32,34 +32,32 @@ export default {
 
                     request.post("/login",formData)
                         .then(function (response) {
-                            console.log(response);
-                            if(response==="SUCCESS_LOGIN_ADMIN"){
+                            if(response==="FAILED"){
+                                //登录失败
+                                alert("用户名或密码错误! 请重新输入");
+                                return that.$router.push({path: '/'});
+                            }
+                            else if(response==="SUCCESS_LOGIN_ADMIN"){
                                 //管理员登录
                                 return that.$router.push({path: '/index_admin'});
                             }
-                            else if(response==="SUCCESS_LOGIN_TEACHER"){
-                                //教师登录
-                                if(response.password.equals("fudan123456")){
-                                    //重置密码
-                                    return that.$router.push({path: '/resetpassword'});
-                                }
-                                else return that.$router.push({path: '/index_teacher'})
+                            else if(response==="SUCCESS_LOGIN_TEACHER_RESETPASSWORD") {
+                                //教师登录首次
+                                return that.$router.push({path: '/resetpassword'});
                             }
-                            // if(response.role===2){
-                            //     //学生登录
-                            //     if(response.password.equals("fudan123456")){
-                            //         //重置密码
-                            //         return that.$router.push({path: '/resetpassword'});
-                            //     }
-                            //     else return that.$router.push({path: '/index_stu'});
-                            // }
-
+                            else if(response==="SUCCESS_LOGIN_TEACHER") {
+                                //教师登录
+                                return that.$router.push({path: '/index_teacher'});
+                            }
+                            else if(response==="SUCCESS_LOGIN_STUDENT_RESETPASSWORD") {
+                                //学生登录首次
+                                return that.$router.push({path: '/resetpassword'});
+                            }
+                            else if(response==="SUCCESS_LOGIN_STUDENT") {
+                                //学生登录
+                                return that.$router.push({path: '/index_stu'});
+                            }
                         })
-                        // .catch(function () {
-                        //     // 处理错误情况
-                        //     alert("用户名或密码错误! 请重新输入");
-                        //     return that.$router.push({path: '/'});
-                        // })
                 }
                 else {
                     return false;
