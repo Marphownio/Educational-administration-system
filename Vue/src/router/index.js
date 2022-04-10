de6522458787315596e27a120789db600edcfde6
Mai Wuyuao <20300290032@fudan.edu.cn>
2022-04-10 10:12:50 +0800

    修改

--- Vue/src/router/index.js
+++ Vue/src/router/index.js
@@ -1,5 +1,5 @@
 import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
-import HomeView from '../views/sys/teacher/index_teacher'
+import HomeView from '../views/Login'
 
 const routes = [
   {
