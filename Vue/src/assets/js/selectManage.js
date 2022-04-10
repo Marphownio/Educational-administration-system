1b60c02274b310849f521e13a321dc09476f6372
Lu Yiyang <20300200013@fudan.edu.cn>
2022-04-10 23:22:21 +0800

    学院、专业表格搜索实现

--- Vue/src/assets/js/selectManage.js
+++ Vue/src/assets/js/selectManage.js
@@ -1,32 +1,33 @@
 import Nav from "@/views/inc/Nav.vue";
 import request from "@/utils/request";
-import {getState} from "core-js/modules/web.url-search-params";
 
 export default {
     name: "selectManage",
     components:{
         Nav
     },
-    data:{
-        isDisabled:false,
+    data(){
+       return{
+           isDisabled1:true,
+           isDisabled2:false,
+       }
     },
     created() {
         this.getStatus();
     },
     methods:{
-        Isdisable1(){
-            return this.isDisabled;
-        },
-        Isdisable2(){
-            return !(this.isDisabled);
-        },
         getStatus(){
             request.get("/admin/courseSelect/status").then(res=>{
                 console.log(res)
                 if(res ==='SUCCESS'){
-                    this.isDisabled=false;
+                    this.isDisabled1=true;
+                    this.isDisabled2=false;
                 }
-                else this.isDisabled=true;
+                else
+                {
+                    this.isDisabled1=false;
+                    this.isDisabled2=true;
+                }
             })
         },
         openSelection(){
@@ -55,7 +56,11 @@ export default {
         },
         closeSelection(){
             let CHANGE=false;
-            request.post("/admin/courseSelect/change",CHANGE).then(res=> {
+            request.post("/admin/courseSelect/change",{
+                params:{
+                    change:CHANGE
+                }
+            }).then(res=> {
                 if(res==='SUCCESS')
                 {
                     this.$message({
