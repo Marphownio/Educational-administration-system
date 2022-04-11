325e7f3be67be4c8ec133f28dd81aea4b60deeb3
Lu Yiyang <20300200013@fudan.edu.cn>
2022-04-11 19:14:39 +0800

    问题修改

--- Vue/src/views/sys/stu/index_stu.vue
+++ Vue/src/views/sys/stu/index_stu.vue
@@ -1,23 +1,23 @@
 <template>
   <Nav></Nav>
-  <p class="Huanyin">欢迎使用教务系统！</p>
-  <table class="Dashboard">
+  <p class="huanyin">欢迎使用教务系统！</p>
+  <table class="dashboard">
     <tr>
       <td class="table_head" colspan="4">
         服务功能:
       </td>
     </tr>
     <tr>
-      <td style="text-align:center" class="Choose">
+      <td style="text-align:center" class="choose">
         <router-link to="/personalinfo" >个人信息</router-link>
       </td>
-      <td style="text-align:center" class="Choose">
+      <td style="text-align:center" class="choose">
         <router-link to="/classselection">选课</router-link>
       </td>
-      <td style="text-align:center" class="Choose">
+      <td style="text-align:center" class="choose">
         <router-link to="/resetpassword" > 重置密码 </router-link>
       </td>
-      <td style="text-align:center" class="Choose">
+      <td style="text-align:center" class="choose">
         <router-link to="/index_stu" > 敬请期待 </router-link>
       </td>
     </tr>
