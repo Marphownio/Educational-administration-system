d7279a797d353ad44c53bfed83851f5d92e8d4cc
Mai Wuyuao <20300290032@fudan.edu.cn>
2022-04-25 13:29:51 +0800

    修改管理员管理时间功能

--- Vue/src/assets/js/timeManage.js
+++ Vue/src/assets/js/timeManage.js
@@ -1,6 +1,7 @@
 import Nav from "@/views/inc/Nav.vue";
 import request from "@/utils/request";
 import ALERTMSG from "@/assets/js/alert";
+import alert from "@/assets/js/alert";
 export default {
     name: "timeManage",
     components:{
@@ -8,35 +9,221 @@ export default {
     },
     data(){
         return{
-            tableData:[],
+            hours1:[
+                {value: '0', label: '0',}, {value: '1', label: '1',},
+                {value: '2', label: '2',}, {value: '3', label: '3',},
+                {value: '4', label: '4',}, {value: '5', label: '5',},
+                {value: '6', label: '6',}, {value: '7', label: '7',},
+                {value: '8', label: '8',}, {value: '9', label: '9',},
+                {value: '10', label: '10',}, {value: '11', label: '11',},
+                {value: '12', label: '12',}, {value: '13', label: '13',},
+                {value: '14', label: '14',}, {value: '15', label: '15',},
+                {value: '16', label: '16',}, {value: '17', label: '17',},
+                {value: '18', label: '18',}, {value: '19', label: '19',},
+                {value: '20', label: '20',}, {value: '21', label: '21',},
+                {value: '22', label: '22',}, {value: '23', label: '23',},
+            ],
+            hours2:[
+                {value: '0', label: '0',}, {value: '1', label: '1',},
+                {value: '2', label: '2',}, {value: '3', label: '3',},
+                {value: '4', label: '4',}, {value: '5', label: '5',},
+                {value: '6', label: '6',}, {value: '7', label: '7',},
+                {value: '8', label: '8',}, {value: '9', label: '9',},
+                {value: '10', label: '10',}, {value: '11', label: '11',},
+                {value: '12', label: '12',}, {value: '13', label: '13',},
+                {value: '14', label: '14',}, {value: '15', label: '15',},
+                {value: '16', label: '16',}, {value: '17', label: '17',},
+                {value: '18', label: '18',}, {value: '19', label: '19',},
+                {value: '20', label: '20',}, {value: '21', label: '21',},
+                {value: '22', label: '22',}, {value: '23', label: '23',},
+            ],
+            minutes1:[
+                {value: '0', label: '0',}, {value: '1', label: '1',},
+                {value: '2', label: '2',}, {value: '3', label: '3',},
+                {value: '4', label: '4',}, {value: '5', label: '5',},
+                {value: '6', label: '6',}, {value: '7', label: '7',},
+                {value: '8', label: '8',}, {value: '9', label: '9',},
+                {value: '10', label: '10',}, {value: '11', label: '11',},
+                {value: '12', label: '12',}, {value: '13', label: '13',},
+                {value: '14', label: '14',}, {value: '15', label: '15',},
+                {value: '16', label: '16',}, {value: '17', label: '17',},
+                {value: '18', label: '18',}, {value: '19', label: '19',},
+                {value: '20', label: '20',}, {value: '21', label: '21',},
+                {value: '22', label: '22',}, {value: '23', label: '23',},
+                {value: '24', label: '24',}, {value: '25', label: '25',},
+                {value: '26', label: '26',}, {value: '27', label: '27',},
+                {value: '28', label: '28',}, {value: '29', label: '29',},
+                {value: '30', label: '30',}, {value: '31', label: '32',},
+                {value: '32', label: '32',}, {value: '33', label: '33',},
+                {value: '34', label: '34',}, {value: '35', label: '35',},
+                {value: '36', label: '36',}, {value: '37', label: '37',},
+                {value: '38', label: '38',}, {value: '39', label: '39',},
+                {value: '40', label: '40',}, {value: '41', label: '41',},
+                {value: '42', label: '42',}, {value: '43', label: '43',},
+                {value: '44', label: '44',}, {value: '45', label: '45',},
+                {value: '46', label: '46',}, {value: '47', label: '47',},
+                {value: '48', label: '48',}, {value: '49', label: '49',},
+                {value: '50', label: '50',}, {value: '51', label: '51',},
+                {value: '52', label: '52',}, {value: '53', label: '53',},
+                {value: '54', label: '54',}, {value: '55', label: '55',},
+                {value: '56', label: '56',}, {value: '57', label: '57',},
+                {value: '58', label: '58',}, {value: '59', label: '59',},
+            ],
+            minutes2:[
+                {value: '0', label: '0',}, {value: '1', label: '1',},
+                {value: '2', label: '2',}, {value: '3', label: '3',},
+                {value: '4', label: '4',}, {value: '5', label: '5',},
+                {value: '6', label: '6',}, {value: '7', label: '7',},
+                {value: '8', label: '8',}, {value: '9', label: '9',},
+                {value: '10', label: '10',}, {value: '11', label: '11',},
+                {value: '12', label: '12',}, {value: '13', label: '13',},
+                {value: '14', label: '14',}, {value: '15', label: '15',},
+                {value: '16', label: '16',}, {value: '17', label: '17',},
+                {value: '18', label: '18',}, {value: '19', label: '19',},
+                {value: '20', label: '20',}, {value: '21', label: '21',},
+                {value: '22', label: '22',}, {value: '23', label: '23',},
+                {value: '24', label: '24',}, {value: '25', label: '25',},
+                {value: '26', label: '26',}, {value: '27', label: '27',},
+                {value: '28', label: '28',}, {value: '29', label: '29',},
+                {value: '30', label: '30',}, {value: '31', label: '32',},
+                {value: '32', label: '32',}, {value: '33', label: '33',},
+                {value: '34', label: '34',}, {value: '35', label: '35',},
+                {value: '36', label: '36',}, {value: '37', label: '37',},
+                {value: '38', label: '38',}, {value: '39', label: '39',},
+                {value: '40', label: '40',}, {value: '41', label: '41',},
+                {value: '42', label: '42',}, {value: '43', label: '43',},
+                {value: '44', label: '44',}, {value: '45', label: '45',},
+                {value: '46', label: '46',}, {value: '47', label: '47',},
+                {value: '48', label: '48',}, {value: '49', label: '49',},
+                {value: '50', label: '50',}, {value: '51', label: '51',},
+                {value: '52', label: '52',}, {value: '53', label: '53',},
+                {value: '54', label: '54',}, {value: '55', label: '55',},
+                {value: '56', label: '56',}, {value: '57', label: '57',},
+                {value: '58', label: '58',}, {value: '59', label: '59',},
+            ],
+            number:'',
+            tableDatatime:[
+                {
+                },
+            ],
             dialogVisible1:false,
             applyform:{
-                classTimeId: '',
-                time:'',
+                classTimeId:'',
+                startTimeHour:'',
+                startTimeMin:'',
+                endTimeHour:'',
+                endTimeMin:'',
             },
             ruleForm1:{
-                classTimeId: '',
-                time:'',
+                classTimeId:'',
+                startTimeHour:'',
+                startTimeMin:'',
+                endTimeHour:'',
+                endTimeMin:'',
             },
             editRules1:({
-                classTimeId:[{required: true, message: '请新课次', trigger: 'blur'}],
-                time:[{required: true, message: '请输入本课次时间', trigger: 'blur'}],
+                startTimeHour:[{required: true, message: '请选择课次开始时', trigger: 'blur'}],
+                startTimeMin:[{required: true, message: '请选择课次开始分', trigger: 'blur'}],
+                endTimeHour:[{required: true, message: '请选择课次结束时', trigger: 'blur'}],
+                endTimeMin:[{required: true, message: '请选择课次结束分', trigger: 'blur'}],
             }),
             dialogVisible2:false,
             ruleForm2:{
-                classTimeId: '',
-                time:'',
+                classTimeId:'',
+                startTimeHour:'',
+                startTimeMin:'',
+                endTimeHour:'',
+                endTimeMin:'',
+            },
+            lastclass:{
+                endHour:'',
+                endMin:'',
+            },
+            nextclass:{
+                startHour:'24',
+                startMin:'60',
             },
             editRules2:({
-                classTimeId:[{required: true, message: '请新课次', trigger: 'blur'}],
-                time:[{required: true, message: '请输入本课次时间', trigger: 'blur'}],
+                startTimeHour:[{required: true, message: '请选择课次开始时', trigger: 'blur'}],
+                startTimeMin:[{required: true, message: '请选择课次开始分', trigger: 'blur'}],
+                endTimeHour:[{required: true, message: '请选择课次结束时', trigger: 'blur'}],
+                endTimeMin:[{required: true, message: '请选择课次结束分', trigger: 'blur'}],
             }),
+            dialogVisible0:false,
+            dialogVisible00:false,
         }
+    },
+    computed:{
     },
     created(){
         this.getinfor();
     },
     methods:{
+        getlastinfor(index){
+
+        },
+        getnextinfor(index){
+
+        },
+        applynew(){
+            const that=this;
+            that.dialogVisible1=true;
+            that.ruleForm1.classTimeId=that.number+1;
+            that.getlastinfor(that.ruleForm1.classTimeId);
+            that.apllytimeedit();
+        },
+        apllytimeedit(){
+            const that=this;
+            //initial
+            for(let i=0;i<that.hours1.length;i++){
+                that.hours1[i].disabled=false;
+                that.hours2[i].disabled=false;
+            }
+            for(let i=0;i<that.minutes1.length;i++){
+                that.minutes1[i].disabled=false;
+                that.minutes2[i].disabled=false;
+            }
+            //starthours
+            for(let i=0;i<that.lastclass.endHour;i++){
+                that.hours1[i].disabled=true;
+            }
+            if(that.ruleForm1.endTimeHour!==''){
+                for(let i=that.ruleForm1.endTimeHour;i<that.hours1.length;i++){
+                    if(i==that.ruleForm1.endTimeHour){
+                        continue;
+                    }
+                    that.hours1[i].disabled=true;
+                }
+            }
+            //startmin
+            if(that.ruleForm1.startTimeHour==that.lastclass.endHour){
+                for(let i=0;i<=that.lastclass.endMin;i++){
+                    that.minutes1[i].disabled=true;
+                }
+            }
+            if(that.ruleForm1.startTimeHour==that.ruleForm1.endTimeHour&&that.ruleForm1.endTimeMin!==''){
+                for(let i=that.ruleForm1.endTimeMin;i<that.minutes1.length;i++){
+                    that.minutes1[i].disabled=true;
+                }
+            }
+            //endhour
+            for(let i=0;i<that.ruleForm1.startTimeHour;i++){
+                that.hours2[i].disabled=true;
+            }
+            //endmin
+            if(that.ruleForm1.endTimeHour==that.ruleForm1.startTimeHour){
+                for(let i=0;i<=that.ruleForm1.startTimeMin;i++){
+                    that.minutes2[i].disabled=true;
+                }
+            }
+            // console.log(that.ruleForm1);
+            if(that.ruleForm1.startTimeHour==that.ruleForm1.endTimeHour){
+                if(that.ruleForm1.endTimeMin!=''&&that.ruleForm1.startTimeMin>=that.ruleForm1.endTimeMin){
+                    console.log("");
+                    that.ruleForm1.endTimeMin='';
+                }
+            }
+        },
         submit_apply:function(){
             this.$refs.ruleForm1.validate((valid) => {
                     if (valid) {
@@ -49,26 +236,98 @@ export default {
                             .then(function (response) {
                                 ALERTMSG.show(that,true,"课次新增成功!","success");
                                 that.getinfor();
+                                that.getSummaries();
                                 }, function (err) {
                                 ALERTMSG.show(that,true,"课次新增失败！请重新尝试！","error");
                                 that.getinfor();
+                                that.getSummaries();
                                 return false;
                             });
                     }
                     else{
                         return false;
                     }
-        })
+            })
         },
         getinfor:function(){
             request.get("/classTime/list").then(res=>{
-                this.tableData= res;
+                this.tableDatatime= res;
             })
         },
-        edittime(row){
+        getSummaries(param){
+            const { columns, data } = param;
+            this.number=data.length;
+        },
+        openedit(row,index){
             this.dialogVisible2=true;
-            this.ruleForm2.time=row.time;
-            this.ruleForm2.classTimeId=row.classTimeId;
+            this.ruleForm2.classTimeId=index;
+            this.ruleForm2.startTimeHour=row.startTimeHour;
+            this.ruleForm2.startTimeMin=row.startTimeMin;
+            this.ruleForm2.endTimeHour=row.endTimeHour;
+            this.ruleForm2.endTimeMin=row.startTimeMin;
+            this.getlastinfor(index);
+            this.getnextinfor(index);
+            this.edittime();
+        },
+        edittime(){
+            const that=this;
+            //initial
+            for(let i=0;i<that.hours1.length;i++){
+                that.hours1[i].disabled=false;
+                that.hours2[i].disabled=false;
+            }
+            for(let i=0;i<that.minutes1.length;i++){
+                that.minutes1[i].disabled=false;
+                that.minutes2[i].disabled=false;
+            }
+            //starthours
+            for(let i=0;i<that.lastclass.endHour;i++){
+                that.hours1[i].disabled=true;
+            }
+            for(let i=that.ruleForm2.endTimeHour;i<that.hours1.length;i++){
+                if(i==that.ruleForm2.endTimeHour){
+                    continue;
+                }
+                that.hours1[i].disabled=true;
+            }
+            //startmin
+            if(that.ruleForm2.startTimeHour==that.lastclass.endHour){
+                for(let i=0;i<=that.lastclass.endMin;i++){
+                    that.minutes1[i].disabled=true;
+                }
+            }
+            if(that.ruleForm2.startTimeHour==that.ruleForm2.endTimeHour&&that.ruleForm2.endTimeMin!==''){
+                for(let i=that.ruleForm2.endTimeMin;i<that.minutes1.length;i++){
+                    that.minutes1[i].disabled=true;
+                }
+            }
+            //endhour
+            for(let i=0;i<that.ruleForm2.startTimeHour;i++){
+                that.hours2[i].disabled=true;
+            }
+            for(let i=that.nextclass.startHour;i<that.hours2.length;i++){
+                if(i==that.nextclass.startHour){
+                    continue;
+                }
+                that.hours2[i].disabled=true;
+            }
+            //endmin
+            if(that.ruleForm2.endTimeHour==that.ruleForm2.startTimeHour){
+                for(let i=0;i<=that.ruleForm2.startTimeMin;i++){
+                    that.minutes2[i].disabled=true;
+                }
+            }
+            if(that.ruleForm2.endTimeHour==that.nextclass.startHour){
+                for(let i=that.ruleForm2.endTimeMin;i<that.minutes2.length;i++){
+                    that.minutes2[i].disabled=true;
+                }
+            }
+            if(that.ruleForm2.startTimeHour==that.ruleForm2.endTimeHour){
+                if(that.ruleForm2.endTimeMin!=''&&that.ruleForm2.startTimeMin>=that.ruleForm2.endTimeMin){
+                    that.ruleForm2.endTimeMin='';
+                }
+            }
+
         },
         submitEdit(){
             const that=this;
@@ -93,19 +352,30 @@ export default {
                 }
             })
         },
-        deletetime(row){
+        deletetime(){
+            if(this.number===0){
+                this.dialogVisible00=true;
+                return false;
+            }
+            else{
+                this.dialogVisible0=true;
+                return true;
+            }
+        },
+        submitdelete() {
             const that=this;
-            let timeid=row.classTimeId;
+            let timeid=that.number;
             request.delete("/classTime/delete/"+timeid)
                 .then(function (response) {
                     ALERTMSG.show(that,true,"课次删除成功!","success");
                     that.getinfor();
+                    that.getSummaries();
                 }, function (err) {
                     ALERTMSG.show(that,true,"课次删除失败！请重新尝试！","error");
                     that.getinfor();
+                    that.getSummaries();
                     return false;
                 });
-        }
-
+        },
     }
 }
--- Vue/src/views/sys/admin/timeManage.vue
+++ Vue/src/views/sys/admin/timeManage.vue
@@ -4,25 +4,61 @@
     <div class="showlist">
       <el-form id="requirebtn" :inline="true" >
         <el-form-item>
-          <el-button type="primary" @click="dialogVisible1=true">申请新课次</el-button>
+          <el-button type="primary" @click="applynew()">申请新课次</el-button>
+          <el-button type="danger" @click="deletetime()">删除最后课次</el-button>
         </el-form-item>
       </el-form>
-    <el-table :data="tableData"
+    <el-table :data="tableDatatime"
+              :summary-method="getSummaries"
+              show-summary
               :header-cell-style="{'text-align':'center'}"
               :cell-style="{'text-align':'center'}"
               >
-      <el-table-column prop="classTimeId" label="课次编号" width="350px" />
-      <el-table-column prop="time" label="时间" />
-      <el-table-column v-slot="scope" fixed="right" prop="icon" label="操作" width="350px">
-        <el-button type="primary" @click="edittime(scope.row)">编辑</el-button>
-        <el-popconfirm @confirm="deletetime(scope.row)" title="确认删除该课次吗？">
-          <template #reference>
-            <el-button type="danger">删除</el-button>
-          </template>
-        </el-popconfirm>
+      <el-table-column prop="index" label="课次编号" type="index" width="180px"/>
+      <el-table-column prop="classTimeId" v-if="false"/>
+      <el-table-column prop="startTimeHour,startTimeMin" label="开始时间">
+        <template v-slot="scope">
+          {{ scope.row.startTimeHour }}:{{ scope.row.startTimeMin }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="endTimeHour,endTimeMin" label="结束时间">
+        <template v-slot="scope">
+          {{ scope.row.endTimeHour }}:{{ scope.row.endTimeMin }}
+        </template>
+      </el-table-column>>
+      <el-table-column v-slot="scope" fixed="right" prop="icon" label="操作" width="180px">
+        <el-button type="primary" @click="openedit(scope.row,scope.$index+1)">编辑</el-button>
       </el-table-column>
     </el-table>
     </div>
+
+
+    <el-dialog
+        v-model="dialogVisible0"
+        title="删除课次"
+        width="600px"
+    >
+      <div class="deletedia">
+        <h1>您确认要删除第{{this.number}}节课吗</h1>
+        <div>
+          <el-button type="primary" @click="dialogVisible0=false" >取消删除</el-button>
+          <el-button type="danger" @click="submitdelete()" >确认删除</el-button>
+        </div>
+      </div>
+
+
+    </el-dialog>
+
+    <el-dialog
+        v-model="dialogVisible00"
+        title="删除课次"
+        width="400px"
+    >
+      <div class="deletedia">
+        <h1>当前无可删除课次！</h1>
+        <el-button type="primary" @click="dialogVisible00=false" >关闭</el-button>
+      </div>
+    </el-dialog>
 
 
     <el-dialog
@@ -36,20 +72,68 @@
           :model="ruleForm1"
           :rules="editRules1"
           label-width="100px"
-          class="demo-ruleForm"
+          class="ruleformdia"
       >
         <el-form-item label="课次:" prop="classTimeId">
-          <el-input v-model="ruleForm1.classTimeId"></el-input>
+          <el-input v-model="ruleForm1.classTimeId" readonly></el-input>
         </el-form-item>
-        <el-form-item label="课堂时间:" prop="time">
-          <el-input v-model="ruleForm1.time"></el-input>
-        </el-form-item>
+        <div class="settime">
+          <el-form-item label="起始时:" prop="startTimeHour">
+            <el-select id="applyStartHour" v-model="ruleForm1.startTimeHour" placeholder="请选择" @change="apllytimeedit">
+              <el-option
+                  v-for="item in hours1"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                  :disabled="item.disabled"
+              />
+            </el-select>
+          </el-form-item>
+          <el-form-item label="起始分:" prop="startTimeMin">
+            <el-select id="applyStartMin" v-model="ruleForm1.startTimeMin" placeholder="请选择" @change="apllytimeedit" :disabled="ruleForm1.startTimeHour==''?true:false">
+              <el-option
+                  v-for="item in minutes1"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                  :disabled="item.disabled"
+              />
+            </el-select>
+          </el-form-item>
+        </div>
+        <div class="settime">
+          <el-form-item label="结束时:" prop="endTimeHour">
+            <el-select id="applyEndHour" v-model="ruleForm1.endTimeHour" placeholder="请选择" @change="apllytimeedit" :disabled="ruleForm1.startTimeMin==''?true:false">
+              <el-option
+                  v-for="item in hours2"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                  :disabled="item.disabled"
+              />
+            </el-select>
+          </el-form-item>
+          <el-form-item label="结束分:" prop="endTimeMin">
+            <el-select id="applyEndMin" v-model="ruleForm1.endTimeMin" placeholder="请选择" @change="apllytimeedit" :disabled="ruleForm1.endTimeHour==''?true:false">
+              <el-option
+                  v-for="item in minutes2"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                  :disabled="item.disabled"
+              />
+            </el-select>
+          </el-form-item>
+        </div>
+
 
         <el-form-item>
           <el-button type="primary" @click="submit_apply()" >确认新增</el-button>
         </el-form-item>
       </el-form>
     </el-dialog>
+
+
 
     <el-dialog
         v-model="dialogVisible2"
@@ -67,9 +151,54 @@
         <el-form-item label="课次:" prop="classTimeId">
           <el-input v-model="ruleForm2.classTimeId" readonly></el-input>
         </el-form-item>
-        <el-form-item label="课堂时间:" prop="time">
-          <el-input v-model="ruleForm2.time"></el-input>
-        </el-form-item>
+        <div class="settime">
+          <el-form-item label="起始时:" prop="startTimeHour">
+            <el-select id="editStartHour" v-model="ruleForm2.startTimeHour" placeholder="请选择" @change="edittime()">
+              <el-option
+                  v-for="item in hours1"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                  :disabled="item.disabled"
+              />
+            </el-select>
+          </el-form-item>
+          <el-form-item label="起始分:" prop="startTimeMin">
+            <el-select id="editStartMin" v-model="ruleForm2.startTimeMin" placeholder="请选择" @change="edittime()">
+              <el-option
+                  v-for="item in minutes1"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                  :disabled="item.disabled"
+              />
+            </el-select>
+          </el-form-item>
+        </div>
+        <div class="settime">
+          <el-form-item label="结束时:" prop="endTimeHour">
+            <el-select id="editEndHour" v-model="ruleForm2.endTimeHour" placeholder="请选择" @change="edittime()">
+              <el-option
+                  v-for="item in hours2"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                  :disabled="item.disabled"
+              />
+            </el-select>
+          </el-form-item>
+          <el-form-item label="结束分:" prop="endTimeMin">
+            <el-select id="editEndMin" v-model="ruleForm2.endTimeMin" placeholder="请选择" @change="edittime()">
+              <el-option
+                  v-for="item in minutes2"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                  :disabled="item.disabled"
+              />
+            </el-select>
+          </el-form-item>
+        </div>
 
         <el-form-item>
           <el-button type="primary" @click="submitEdit()" >确认修改</el-button>
@@ -77,13 +206,12 @@
       </el-form>
     </el-dialog>
 
-
     </body>
 </template>
-
-<script src="../../../assets/js/timeManage.js" type="text/javascript">
+<script defer=true src="../../../assets/js/timeManage.js" type="text/javascript">
 
 </script>
+
 
 <style  scoped>
    @import "../../../assets/css/timeManage.css";
